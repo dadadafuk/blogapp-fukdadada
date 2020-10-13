fbb9bb04c5cbff3d9293b459f91e72e22868fc41
yfukuda <steptime0226@gmail.com>
2020-10-13 22:11:29 +0900

    日付の表示

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,9 @@ class Article < ApplicationRecord
   # コロンの位置を間違えない！！
   validates :title, presence: true
   validates :content, presence: true
+
+  def display_created_at
+    I18n.l(self.created_at, format: :default)
+  end
+
 end
