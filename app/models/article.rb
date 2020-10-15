95bedaffb04bf0d613e7b598da91d1ef357443f4
yfukuda <steptime0226@gmail.com>
2020-10-15 22:52:40 +0900

    length追加

--- app/models/article.rb
+++ app/models/article.rb
@@ -11,7 +11,10 @@
 class Article < ApplicationRecord
   # コロンの位置を間違えない！！
   validates :title, presence: true
+  validates :title, length: { minimum: 2, maximum: 100 }
+
   validates :content, presence: true
+  validates :content, length: { minimum: 10 }
 
   def display_created_at
     I18n.l(self.created_at, format: :default)
